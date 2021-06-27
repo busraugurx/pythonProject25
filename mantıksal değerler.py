d9fa65a163fadb78cad243211e387c5bda733387
busraugurx <busraugur934@gmail.com>
2021-06-27 18:10:15 +0300

    Initial commit

---
+++ mantıksal değerler.py
@@ -0,0 +1,52 @@
+'''# and operatoru
+print(1>2 or "murat"!="murat")'''
+"""#print(not 2==2)
+print(not (4.65>4.654 or("büş"=="büş" and 2!=2)))"""
+"""a=int(input("bir sayı giriniz:"))
+print("girdiğiniz sayı: ",a)"""
+'''if(a > 0):
+    print("Girdiğiniz sayı pozitif.")'''
+
+"""yaş= int(input("yaşınızı girin: "))
+print("yaşınız: ", yaş)
+if(yaş<18):
+    print("Mekana Giremezsiniz!!!")
+else:
+    print("Mekana Hoşgeldiniz!")"""
+
+"""sayi=int(input("bir sayı giriniz: "))
+print("girdiğiniz sayi: ", sayi)"""
+
+"""if (sayi<0):
+    print("Girmiş Olduğunuz Sayi Negatiftir.")
+elif(sayi==0):
+    print("Girmiş Oldugunuz sayi sıfırdır.")
+else:
+    print("Girmiş Olduğunuz Sayi Pozitiftir.")"""
+
+#NOT HESAPLAMA SİSTEMİ
+
+note=float(input("NOTUNUZU GİRİN: "))
+
+if(note>100):
+    print("GEÇERSİZ NOT")
+elif(note>=95):
+    print("AA")
+elif(note>=90):
+    print("BA")
+elif(note>=80):
+    print("BB")
+elif(note>=70):
+    print("CB")
+elif(note>=60):
+    print("CC")
+elif(note>=50):
+    print("DC")
+elif(note>40):
+    print("DD")
+elif( note>0 and note<=40):
+    print("FF : KALDINIZ!")
+
+
+
+
